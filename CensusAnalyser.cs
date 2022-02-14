2ca39d4261107e4b0cf0f50186a3af35f0a5fb56
Pankaj Kumar <pk110495@gmail.com>
2022-02-14 20:39:11 +0530

    added test case and census analyser

---
+++ CensusAnalyser.cs
@@ -0,0 +1,28 @@
+﻿using IndianStateCensusAnalyzer.DTO;
+using System;
+namespace IndianStateCensusAnalyzer
+{
+
+    public class CensusAnalyser
+    {
+
+        public static int a = 10;
+        public enum Country
+        {
+            INDIA, US, BRAZIL
+        }
+
+
+        Dictionary<string, CensusDTO> dataMap;
+        public Dictionary<string, CensusDTO> LoadCensusData(string csvFilePath, Country country, string dataHeaders)
+        {
+            //CensusAnalyser obj = new CensusAnalyser();
+            //Console.WriteLine(obj.a);
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+
+    }
+
+}
+
